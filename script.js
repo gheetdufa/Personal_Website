6d0f7345b8c16502a79f59b3537c166de31bc997
Dheer Guda <gudadheer@gmail.com>
2024-07-27 12:38:35 -0400

    all new changes

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-function toggleMenu(){
-    const menu = document.querySelector(".menu_links")
-    //function already in javascript, we are just selecting the code we created in index.html
-    const icon = document.querySelector(".hamburger_icon")
-    menu.classList.toggle("open")
-    icon.classList.toggle("open")
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
 }
+
---
+++ start.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let intro = document.querySelector('.intro');
+    let startSpans = document.querySelectorAll('.start');
+  
+    setTimeout(() => {
+      startSpans.forEach((span, idx) => {
+        setTimeout(() => {
+          span.classList.add('active');
+        }, (idx + 1) * 400);
+      });
+  
+      setTimeout(() => {
+        startSpans.forEach((span, idx) => {
+          setTimeout(() => {
+            span.classList.remove('active');
+            span.classList.add('fade');
+          }, (idx + 1) * 50);
+        });
+      }, 2000);
+  
+      setTimeout(() => {
+        intro.style.top = '-100vh';
+      }, 2300);
+    });
+  });
+  
